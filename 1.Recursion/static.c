842e810b798d07989a2f243c775899efba95b79e
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2020-04-16 17:35:44 +0530

    Add files via upload

---
+++ 1.Recursion/static.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fun(int n)
+{
+    static int x;
+    if(n>0)
+    {
+        x++;
+        return fun(n-1)+x;
+    }
+    return 0;
+}
+
+int main()
+{
+    int r;
+    r=fun(5);
+    printf("%d",r);
+    return 0;
+}
+
